ae1f28798fd3f6f694c2f4e91a7f9d511125160b
juan <jjsorianoe@gmail.com>
2017-11-26 22:33:36 +0100

    estimate pending

--- Session 2/kalman.py
+++ Session 2/kalman.py
@@ -11,6 +11,9 @@ Q: The process noise covariance marix.
 B: The input effect matrix.
 U: The control input.
 '''
+# simulation of the data
+Xtest =  reduce(lambda c, x: c + [c[-1] + 10], range(100),[0])[1:]
+
 def prediction(X, P, A, Q, B, U):
     X = np.dot(A, X) + np.dot(B, U)
     P = np.dot(A, np.dot(P, A.T)) + Q # (A - no transpose because it is a scalar)
@@ -19,17 +22,15 @@ def prediction(X, P, A, Q, B, U):
 def update(X, P, H, K, Y, R):
     MP = np.dot(H, X) # measurement prediction
     residual = Y - MP # the residual of the prediction
-    MPC = np.dot(1, np.dot(P, 1))  + R    # measurement prediction covariance ( C- no transpose because it is a scalar)
-    K = np.dot(P, np.dot(1, np.linalg.inv(MPC))) # kalman (C - no transpose because it is a scalar and np.inv()no used for escalars)
+    MPC = np.dot(H, np.dot(P, H))  + R    # measurement prediction covariance ( C- no transpose because it is a scalar)
+    K = np.dot(P, np.dot(H, np.linalg.inv(MPC))) # kalman (C - no transpose because it is a scalar and np.inv()no used for escalars)
     X = X + np.dot(K, residual) # Updated State Estimate
-    print(np.dot(K, residual))
     P = P - np.dot(K, np.dot(K, np.dot(MPC, K.T))) # Updated State Covariance
     return (X,P,K)
 iterations = 100
 # Q = np.random.normal(10, 0.0, iterations)
 Q = np.matrix([[0.0, 0.0],[0.0, 1.0]])
 R = np.random.normal(0, 1.0, iterations)
-print(R)
 dt = 1
 # Initialization of the values of the variable
 # R = []
@@ -55,7 +56,9 @@ C = np.matrix([[1, 0]]) # The noisy measurement is direct
 G = np.matrix([[(dt**2)/2],[ dt ] ])
 B = G
 H = np.matrix([[1., 0], [0, 1.]])
-Y = C*X + R[0]
+Hi = 1
+# Y = C*X 
+Y = Xtest[0]
 # Q = 10e-5 # Process variance
 # R = 0.01 # given from the problem as a constant noise
 K = np.zeros(iterations) # gain or blending factor initialization
@@ -65,25 +68,25 @@ x = np.matrix([[0.0], [0.0]])
 
 dist = []
 p_dist = []
-dist.append((X.item(0,0),X.item(1,0)))
-p_dist.append((Y.item(0,0)))
+dist.append((X.item(0,0)))
+# p_dist.append((Y.item(0,0)))
 for i in range(1, iterations):
-    print(X)
     # prediction
     X, P = prediction(X, P, A, Q, B, U)
     # update the values
-    X, P, K = update(X, P, H, K, Y, R[i])
-    dist.append((X.item(0,0),X.item(1,0)))
-    Y = C*X +R[i]
-    p_dist.append((Y.item(0,0)))
+    X, P, K = update(X, P, Hi, K, Y, R[i])
+  
+    Y = Xtest[i]
+    # p_dist.append((Y.item(0,0)))
    # MSE[i] = ((x[:i] - X[:i])**2).mean() # Mean square error calculated at iteration i
 
-velo = list(map(lambda x: x[1], dist))
-dista = list(map(lambda x: x[0], dist))
-distance = reduce(lambda c, x: c + [c[-1] + x[0]], dist,[0])[1:]
+print(dist)
+# velo = list(map(lambda x: x[1], dist))
+# dista = list(map(lambda x: x[0], dist))
+# distance = reduce(lambda c, x: c + [c[-1] + x[0]], dist,[0])[1:]
 pylab.figure()
-pylab.plot(vel,label='noisy measurements', color='r')
-#pylab.plot(distance, velo,'b-',label='voltage estimate')
+pylab.plot(Xtest,list(map(lambda x: 10, range(100))),label='true measurements', color='r')
+pylab.plot(dist,'b-',label='Velocity estimate')
 # pylab.plot(x,color='g',label='truth value')
 pylab.legend()
 pylab.xlabel('Distance')
