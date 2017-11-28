2223e801668813e3552a3c1ba12b11816c72b130
juan <jjsorianoe@gmail.com>
2017-11-28 18:39:51 +0100

    refactoring the model

--- Session 2/kalman.py
+++ Session 2/kalman.py
@@ -14,10 +14,13 @@ U: The control input.
 # simulation of the data
 Xreal_distance =  reduce(lambda c, x: c + [c[-1] + 10], range(100),[0])[1:]
 Xreal_speed = list(map(lambda x: 10, range(100)))
-Z = np.vstack((Xreal_distance, Xreal_speed))
+real = np.vstack((Xreal_distance, Xreal_speed))
 
-def prediction(X, P, A, Q, B, U):
-    X = np.dot(A, X) + np.dot(B, U)
+'''
+Kalman Definition functions
+'''
+def prediction(X, P, A, Q, B, U, W):
+    X = np.dot(A, X) + W # + np.dot(B, U)
     P = np.dot(A, np.dot(P, A.T)) + Q # (A - no transpose because it is a scalar)
     return X, P
 
@@ -27,21 +30,27 @@ def update(X, P, H, K, Y, R):
     MPC = np.dot(H, np.dot(P, H))  + R    # measurement prediction covariance ( C- no transpose because it is a scalar)
     K = np.dot(P, np.dot(H, np.linalg.inv(MPC))) # kalman (C - no transpose because it is a scalar and np.inv()no used for escalars)
     X = X + np.dot(K, residual) # Updated State Estimate
-    P = P - np.dot(K, np.dot(K, np.dot(MPC, K.T))) # Updated State Covariance
+    P = np.dot((np.identity(P.shape[0]) - np.dot(K, H)), P)# Updated State Covariance # old way: P = P - np.dot(K, np.dot(K, np.dot(MPC, K.T))) 
+    
     return (X,P,K)
 
+
+'''
+values initialization
+'''
 iterations = 100
 dt = 1
+q = 0
 
-# Q = np.random.normal(10, 0.0, iterations)
+wk_distance = np.random.normal(0, np.sqrt(q*((dt**3)/3)),iterations) # noise in the position
+wk_velocity = np.random.normal(0, np.sqrt(q*dt),iterations)         # noise in the velocity
+W = np.vstack((wk_distance, wk_velocity)) # white noise function
+
 Q = np.matrix([[(dt**3)/3, (dt**2/2)],[(dt**2/2), dt]])
 R = np.random.normal(0, 1.0, iterations)
-#     X.append([[0.0], [0.0]])
-
 X = np.matrix([[0.0],[0.0]]) # Initial X values
 P = np.matrix([[1., 0.5], [0.5, 2]])
 
-vel = np.full(100,10)
 A = np.matrix([[1., dt], [0, 1.]]) # The state is a constant
 U = 0 # There is no control input (Aceleration!).
 B = np.matrix([[0.0], [0.0]]) # There is no control input
@@ -50,34 +59,31 @@ G = np.matrix([[(dt**2)/2],[ dt ] ])
 B = G
 H = np.matrix([[1., 0], [0, 1.]])
 Hi = 1
-Y = np.matrix([[Z[0][0]],[Z[1][0]]])
-# Y = C*X 
-# Q = 10e-5 # Process variance
-# R = 0.01 # given from the problem as a constant noise
+Y = np.matrix([[real[0][0]],[real[1][0]]]) + R[0]
 K = np.zeros(iterations) # gain or blending factor initialization
-ISE = np.zeros(iterations) # Instant Square Error
-MSE = np.zeros(iterations) # Mean Square Error
 
 p_distance = []
 p_velocity = []
 p_distance.append((X.item(0,0)))
 p_velocity.append((X.item(1,0)))
+
+'''
+Simulation
+'''
 for i in range(1, iterations):
     # prediction
-    X, P = prediction(X, P, A, Q, B, U)
-    p_distance.append(X.item(0,0))
-    p_velocity.append(X.item(1,0))
+    X, P = prediction(X, P, A, Q, B, U, np.matrix([[W[0][i]],[W[1][i]]]))
+    p_distance.append(X.item(0,0)) # add predicted distance
+    p_velocity.append(X.item(1,0)) # add predicted velocity
     # update the values
     X, P, K = update(X, P, Hi, K, Y, R[i])
-    Y = np.matrix([[Z[0][i]],[Z[1][i]]])
-    # p_dist.append((Y.item(0,0)))
-   # MSE[i] = ((x[:i] - X[:i])**2).mean() # Mean square error calculated at iteration i
+    Y = np.matrix([[real[0][i]],[real[1][i]]]) + R[i] # next measurement value
 
-# velo = list(map(lambda x: x[1], dist))
-# dista = list(map(lambda x: x[0], dist))
-# distance = reduce(lambda c, x: c + [c[-1] + x[0]], dist,[0])[1:]
+'''
+Charts
+'''
 pylab.figure()
-pylab.plot(Z[0],Z[1],label='true measurements', color='r')
+pylab.plot(real[0],real[1],label='true measurements', color='r')
 pylab.plot(p_distance, p_velocity,'b-',label='Velocity estimate')
 # pylab.plot(x,color='g',label='truth value')
 pylab.legend()
