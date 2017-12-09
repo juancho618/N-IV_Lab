d725dd62c773a08fdcff29c837276deb63dc8849
Juan <jjsorianoe@gmail.com>
2017-12-09 15:30:57 +0100

    first working version of the model

--- Session 3/kalman.py
+++ Session 3/kalman.py
@@ -4,6 +4,12 @@ from functools import reduce
 from filterpy.common import Q_discrete_white_noise
 import math
 
+
+dt = 0.05
+r = 5
+q=0.1
+
+
 class RadarSim(object):
     def __init__(self, dt, pos, velx, alt, vely):
         self.pos = pos
@@ -13,8 +19,8 @@ class RadarSim(object):
         self.dt = dt
 
     def get_range(self):
-        self.velx = self.velx + .1*np.random.randn() # TODO: replace with the process noise!
-        self.vely = self.vely + .1*np.random.randn() # TODO: replace with the process noise!
+        self.velx = self.velx + np.sqrt(q)*np.random.randn() 
+        self.vely = self.vely + np.sqrt(q)*np.random.randn() 
         self.alt =self.alt + self.vely*self.dt
         self.pos =self.pos + self.velx*self.dt
 
@@ -23,44 +29,25 @@ class RadarSim(object):
         r_distance = math.sqrt(self.pos**2 + self.alt**2)
 
         return(r_distance + err)
-    
-
-
-# datax = []
-# datay = []
-# for i in range(50):
-#     r,x,y = radar.get_range()
-#     datax.append(x)
-#     datay.append(y)
-
-# pylab.figure()
-# pylab.plot(datax, datay, color='g')
-# pylab.show()
-
-'''
-Data generation
-'''
 
 '''
 initial values
 '''
-dt = 1
-r = 5
-X = np.matrix([[0.0],[0.0], [0.0], [0.0]])
+
 A = np.matrix([[1., dt, 0, 0],
                 [0, 1, 0, 0],
                 [0, 0, 1, dt],
                 [0, 0, 0, 1]])
-Q = np.dot( np.sqrt(0.05),np.matrix([[(dt**3)/3, (dt**2)/2, 0, 0],
+Q = np.dot( np.sqrt(q),np.matrix([[(dt**3)/3, (dt**2)/2, 0, 0],
             [(dt**2)/2, dt, 0, 0],
             [0, 0, (dt**3)/3, (dt**2)/2],
             [0, 0, (dt**2)/2, dt]])) 
-P = np.eye(4)*50           
+P = np.linalg.inv(np.diag([r**2,(r**2/dt**2),r**2,(r**2/dt**2)]))
 
 def HJacobian_at(x):
     """ compute Jacobian of H matrix at x """
-    horiz_dist = x[0]
-    altitude = x[2]
+    horiz_dist = x.item(0,0)
+    altitude = x.item(2,0)
     denom = math.sqrt(horiz_dist**2 + altitude**2)
     return np.matrix([[horiz_dist/denom, 0., altitude/denom, 0.]])
 
@@ -68,7 +55,6 @@ def hx(x):
     """ compute measurement for slant range that
     would correspond to state x.
     """
-    print(x)
     return(math.sqrt(x[0]**2 + x[2]**2))
 
 def prediction(X, P, A, Q, B, U):
@@ -78,28 +64,74 @@ def prediction(X, P, A, Q, B, U):
 
 def update(X, P, H, Y, r):
     MP =  hx(X) #np.dot(H, X) # measurement prediction
-    print('mp', MP)
-    print('y', Y)
     residual = Y - MP # the residual of the prediction
-    print('residual', residual)
-    MPC = np.dot(H, np.dot(P, H.T))  + 5 #r  # measurement prediction covariance ( C- no transpose because it is a scalar)
-    print(MPC.shape)
+    MPC = np.dot(H, np.dot(P, H.T))  + r #r  # measurement prediction covariance ( C- no transpose because it is a scalar)
     K = np.dot(P, np.dot(H.T, MPC.item(0,0)**-1)) # kalman (C - no transpose because it is a scalar and np.inv()no used for escalars)
     X = X + np.dot(K, residual) # Updated State Estimate
     P = np.dot((np.identity(P.shape[0]) - np.dot(K, H)), P)# Updated State Covariance # old way: P = P - np.dot(K, np.dot(K, np.dot(MPC, K.T)))
 
     return (X,P,K)
-radar = RadarSim(1, pos=0, velx=100,vely=100, alt=1000)
+
+radar = RadarSim(dt, pos=0, velx=100,alt=1000,vely=100 )
+X = np.matrix([[radar.pos],[radar.velx-10], [radar.alt+100], [radar.vely-10]])
+
+
 
 track = []
 xs = []
-for i in range(100):
-    print(i)
+for i in range(int(20/dt)):
     z = radar.get_range()
-    track.append((radar.pos, radar.alt, radar.velx, radar.vely))
+    track.append((radar.pos, radar.velx, radar.alt, radar.vely))
+    
     H = HJacobian_at(X)
-
     X, P = prediction(X, P, A, Q, 0, 0)
 
     X, P, K = update(X, P, H, z, 0)
     xs.append(X)
+
+def getParameters(x):
+    r = math.sqrt(x[0]**2 + x[2]**2)
+    ang = math.tan(x[2]/x[0])
+
+    return r
+
+time = np.arange(0, len(xs)*dt, dt)
+xs = np.asarray(np.squeeze(xs))
+track_pos = list(map(lambda x: getParameters(x),track)) 
+x_pos = list(map(lambda x: getParameters(x),xs))
+pylab.title('Position of the object from the radar')
+pylab.plot(time, track_pos,  color = 'g', label='Real X position')
+pylab.plot(time,x_pos, color = 'r', label='Estimated X position')
+pylab.xlabel('time [s]')
+pylab.ylabel('Velocity V [m/s]')
+
+track_velx = list(map(lambda x: x[1],track)) 
+x_velx = list(map(lambda x: x[1], xs)) 
+pylab.figure()
+pylab.title('Velocity in axis X')
+pylab.plot(time, track_velx, color='b', label='Real X velocity')
+pylab.plot(time, x_velx, color='r', label='Predicted X velocity')
+pylab.xlabel('time [s]')
+pylab.ylabel('Velocity V [m/s]')
+
+track_vely = list(map(lambda x: x[3],track)) 
+x_vely = list(map(lambda x: x[3], xs)) 
+pylab.figure()
+pylab.title('Velocity in axis Y')
+pylab.plot(time, track_vely, color='b', label='Real Y velocity')
+pylab.plot(time, x_vely, color='r', label='Predicted Y velocity')
+pylab.xlabel('time [s]')
+pylab.ylabel('Velocity V [m/s]')
+
+
+track_alt = list(map(lambda x: x[2],track)) 
+x_alt = list(map(lambda x: x[2], xs)) 
+pylab.figure()
+pylab.title('Y position of the object')
+pylab.plot(time, track_alt, color='b', label='Real y position')
+pylab.plot(time, x_alt, color='r', label='Estimated y position')
+pylab.xlabel('time [s]')
+pylab.ylabel('Velocity V [m/s]')
+
+
+pylab.show()
